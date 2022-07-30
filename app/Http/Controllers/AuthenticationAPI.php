9c8f52330acfbc026074ddc678e4ac598eecc728
Nabil <nabilrt51@gmail.com>
2022-07-30 13:39:16 +0600

    API Links added with React API Authentication for Login

---
+++ app/Http/Controllers/AuthenticationAPI.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Admin;
+use App\Models\Doctor;
+use App\Models\Patient;
+use App\Models\Seller;
+use App\Models\Token;
+use DateTime;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class AuthenticationAPI extends Controller
+{
+    //
+    public function login(Request $req){
+
+        $username = $req->username;
+        $password = $req->password;
+
+        $rec=explode('-',$username);
+
+        if($rec[1]=="D"){
+
+            $doctor = Doctor::whereRaw("BINARY doctor_id = '$username'")->whereRaw("BINARY doctor_pass = '$password'")->first();
+            if($doctor){
+
+                $api_token = Str::random(64);
+                $token = new Token();
+                $token->user_id = $doctor->doctor_id;
+                $token->token = $api_token;
+                $token->token_for="Doctor";
+                $token->created_at = new DateTime();
+                $token->save();
+                return $token;
+
+            }
+        }
+        else if($rec[1]=="P"){
+
+            $patient=Patient::where('patient_id',$req->username)->where('patient_pass',$password)->first();
+            if($patient){
+
+                $api_token = Str::random(64);
+                $token = new Token();
+                $token->user_id = $patient->patient_id;
+                $token->token = $api_token;
+                $token->token_for="Patient";
+                $token->created_at = new DateTime();
+                $token->save();
+                return $token;
+
+            }
+        }
+        else if($rec[1]=="MS"){
+            $seller = Seller::where('seller_id',$req->username)->where('seller_pass',$password)->first();
+            if($seller){
+
+                $api_token = Str::random(64);
+                $token = new Token();
+                $token->user_id = $seller->seller_id;
+                $token->token = $api_token;
+                $token->token_for="Seller";
+                $token->created_at = new DateTime();
+                $token->save();
+                return $token;
+
+            }
+        }
+        else if($rec[1]=="AD"){
+            $admin = Admin::where('admin_id',$req->username)->where('admin_pass',$password)->first();
+            if($admin){
+
+                $api_token = Str::random(64);
+                $token = new Token();
+                $token->user_id = $admin->admin_id;
+                $token->token = $api_token;
+                $token->token_for="Admin";
+                $token->created_at = new DateTime();
+                $token->save();
+                return $token;
+
+            }
+
+        }
+
+        return "No User Found";
+
+    }
+
+    public function logout(){
+
+        $activeUser=Token::where('expired_at',NULL)->first();
+        $activeUser->expired_at=new DateTime();
+        $activeUser->save();
+
+        return "Success";
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthenticationAPI;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login',[AuthenticationAPI::class,'login']);
+Route::get('/logout',[AuthenticationAPI::class,'logout']);
